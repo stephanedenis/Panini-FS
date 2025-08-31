b25f8252c6c6b45656b7a92c81ae5bbc3e2d3eb5
Stéphane Denis <stephane@sdenis.com>
2025-08-31 09:39:39 -0400

    chore(dev): add VS Code tasks and noninteractive env script to avoid pagers

---
+++ scripts/noninteractive_env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Source this file to disable pagers and editors for non-interactive runs
+export GIT_PAGER=cat
+export PAGER=cat
+export LESS=FRX
+export EDITOR=true
+export VISUAL=true
+export GIT_EDITOR=true
+export GIT_SEQUENCE_EDITOR=true
